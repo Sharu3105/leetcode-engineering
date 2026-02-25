3a76d0d72aa4405bcbea89e51215a58e38124aef
SHARATH T E <latheshwar1412@gmail.com>
2026-02-25 21:34:47 +0530

    Create solution.py

---
+++ Medium/15-3Sum/solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def threeSum(self, nums):
+        nums.sort()
+        res = []
+
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
+
+            # Optimization (optional but good)
+            if a > 0:
+                break
+
+            l, r = i + 1, len(nums) - 1
+
+            while l < r:
+                total = a + nums[l] + nums[r]
+
+                if total > 0:
+                    r -= 1
+                elif total < 0:
+                    l += 1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
+        return res
