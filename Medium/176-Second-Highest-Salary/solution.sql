2ffaa4191ae0bf3aae3eea1e00bb7f90a5bcab79
SHARATH T E <latheshwar1412@gmail.com>
2026-03-03 08:03:10 +0530

    Update solution.sql

--- Medium/176-Second-Highest-Salary/solution.sql
+++ Medium/176-Second-Highest-Salary/solution.sql
@@ -1,5 +1,6 @@
-SELECT employee_id
-FROM Employees
-WHERE salary < 30000 
-  AND manager_id NOT IN (SELECT employee_id FROM Employees)
-ORDER BY employee_id;
+SELECT (
+    SELECT DISTINCT salary 
+    FROM Employee 
+    ORDER BY salary DESC 
+    LIMIT 1 OFFSET 1
+) AS SecondHighestSalary;
